c14717d0d726197320f92e4ba24155584c5ce4f4
destroyer <destroyer.dcf@gmail.com>
2025-10-02 19:23:16 +0200

    refactor: remove bottle building from CI workflow

--- Formula/toolchain.rb
+++ Formula/toolchain.rb
@@ -36,7 +36,7 @@ class Toolchain < Formula
       mkdir "build" do
         system "cmake", "..", *std_cmake_args
         system "cmake", "--build", "."
-        bin.install "build/iDSK"
+        bin.install "iDSK"  # ojo: aquí ya no hace falta "build/" porque ya estás en build/
       end
     end
 
