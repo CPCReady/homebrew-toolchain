b14b09f2987c2579bb5bd6fcce9adc682dc974ee
destroyer <destroyer.dcf@gmail.com>
2025-10-02 20:04:45 +0200

    refactor: remove bottle building from CI workflow

--- Formula/toolchain.rb
+++ Formula/toolchain.rb
@@ -1,9 +1,8 @@
 class Toolchain < Formula
   desc "C/C++ utilities for CPC development"
   homepage "https://github.com/CPCReady/toolchain"
-  url "https://github.com/CPCReady/toolchain.git",
-      tag:      "v0.0.7",
-      revision: "baf205e943845cc5336a6c6556e1aebc22b6ab4e" # reemplaza con el SHA real del tag
+  url "https://github.com/CPCReady/toolchain/releases/download/v0.0.7/cpcready-toolchain-v0.0.7.tar.gz"
+  sha256 "58a9b32cda26383d2ecd95f695bee679a40d6f199a63df9fa7cdb4c61a3d55ce"
   license "MIT"
 
   depends_on "gcc" => :build
@@ -68,5 +67,66 @@ class Toolchain < Formula
 
     # opcional: probar que iDSK existe
     assert_predicate bin/"iDSK", :exist?
+  end
+end
+
+
+class Toolchain < Formula
+  desc "C/C++ utilities for CPC development"
+  homepage "https://github.com/CPCReady/toolchain"
+  url "https://github.com/CPCReady/toolchain/releases/download/v0.0.5/cpcready-toolchain-v0.0.5.tar.gz"
+  sha256 "398310940f55d4aa6e92c28b1238e3f46a57b92a0e85bc56dea62cf623d54a50"
+  license "MIT"
+
+  def pour_bottle?
+    false
+  end
+
+  depends_on "gcc" => :build
+  depends_on "make" => :build
+  depends_on "inih"
+
+  def install
+    ENV.append_path "PATH", Formula["gcc"].opt_bin
+    ENV["CC"] = Formula["gcc"].opt_bin/"gcc-15"
+
+    cd "tools/cpc-config" do
+      system Formula["gcc"].opt_bin/"gcc-15", "cpc-config.c", "-o", "cpc-config"
+      bin.install "cpc-config"
+    end
+
+    cd "tools/cpc-ini" do
+      system Formula["gcc"].opt_bin/"gcc-15",
+            "-I#{Formula["inih"].opt_include}",
+            "-L#{Formula["inih"].opt_lib}",
+            "main.c",
+            "-o", "cpc-ini",
+            "-linih"
+      bin.install "cpc-ini"
+    end
+
+    cd "tools/idsk" do
+      build_dir = Pathname.pwd/"build"
+      build_dir.mkpath
+
+      cpp_files = %w[
+        Basic.cpp BitmapCPC.cpp Dams.cpp Desass.cpp endianPPC.cpp
+        GestDsk.cpp getopt_pp.cpp Main.cpp Outils.cpp ViewFile.cpp Ascii.cpp
+      ].map { |f| Pathname.pwd/"src"/f }
+
+      system Formula["gcc"].opt_bin/"g++-15",
+            "-std=c++11", "-O2", "-Wall",
+            *cpp_files,
+            "-o", build_dir/"iDSK"
+
+      bin.install build_dir/"iDSK"
+    end
+
+
+  end
+
+  test do
+    assert_match "Usage:", shell_output("#{bin}/cpc-config 2>&1", 1)
+    assert_predicate bin/"cpc-config", :executable?
   end
 end
