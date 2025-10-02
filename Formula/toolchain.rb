96391c8fcf17d2019b10ec1664ab861a208c3ea0
destroyer <destroyer.dcf@gmail.com>
2025-10-02 18:46:36 +0200

    initial commit

--- Formula/toolchain.rb
+++ Formula/toolchain.rb
@@ -1,25 +1,29 @@
 class Toolchain < Formula
-  desc "C/C++ utilities for CPC development" # Descripción mejorada que no comienza con el nombre
+  desc "C/C++ utilities for CPC development"
   homepage "https://github.com/CPCReady/toolchain"
   url "https://github.com/CPCReady/toolchain/releases/download/v0.0.5/cpcready-toolchain-v0.0.5.tar.gz"
   sha256 "398310940f55d4aa6e92c28b1238e3f46a57b92a0e85bc56dea62cf623d54a50"
   license "MIT"
 
-  # Dependencias ordenadas alfabéticamente
+  def pour_bottle?
+    false
+  end
+
   depends_on "gcc" => :build
   depends_on "make" => :build
 
   def install
+    ENV.append_path "PATH", Formula["gcc"].opt_bin
+    ENV["CC"] = Formula["gcc"].opt_bin/"gcc-15"
+
     cd "tools/cpc-config" do
-      system ENV.cc, "cpc-config.c", "-o", "cpc-config"
+      system Formula["gcc"].opt_bin/"gcc-15", "cpc-config.c", "-o", "cpc-config"
       bin.install "cpc-config"
     end
   end
 
   test do
-    # Test más robusto que verifica la versión en lugar de --help
     assert_match "Usage:", shell_output("#{bin}/cpc-config 2>&1", 1)
-    # También podemos verificar que el binario existe y es ejecutable
     assert_predicate bin/"cpc-config", :executable?
   end
 end
